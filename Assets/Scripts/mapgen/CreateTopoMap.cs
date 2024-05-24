ff436dea05d184c2f107f303bd5778ef1378639c
fishmonkey1 <11984660+fishmonkey1@users.noreply.github.com>
2024-05-24 19:21:44 -0400

    cleanup
    bit of cleanup

--- Assets/Scripts/mapgen/CreateTopoMap.cs
+++ Assets/Scripts/mapgen/CreateTopoMap.cs
@@ -4,35 +4,17 @@ using UnityEngine;
 
 public class CreateTopoMap : MonoBehaviour
 {
-    [SerializeField]private GameObject topoObject;
-
-    private List<Texture2D> texturesList;
-    private Dictionary<Texture2D, int> topoTexturesDict;
-    private TerrainData terrainData;
+    [SerializeField] private GameObject topoObject;
     [SerializeField] private LayerTerrain lt;
-    private int start_x, start_y, end_x, end_y;
 
     private bool debug = false;
     public void createTopoTextures(int start_x, int start_y, int end_x, int end_y, bool deform)
     {   
         
-        terrainData = lt.terrain.terrainData;
-        topoObject = lt.topoObject;
-
-        float bandDistance = lt.depth / lt.numberOfTopoLevels; //size of bands to create
-        
-        List<float> bands = new List<float>();
-
-        float lerpStep = 1f / (float)lt.numberOfTopoLevels;
-        float currLerp = 0f;
-
+        // make list of all the colors
         List<Color> colors = new List<Color>();
-        Texture2D texture = new Texture2D(lt.X, lt.Y);
-        Renderer rend = topoObject.GetComponent<Renderer>(); 
-
-        Color[] colorMap = new Color[lt.X * lt.Y];
-
-        //make list of all the colors
+        float currLerp = 0f;
+        float lerpStep = 1f / (float)lt.numberOfTopoLevels;
         for (int i = 0; i < lt.numberOfTopoLevels; i++)
         {
             var tileColor = Color.Lerp(lt.topoColor1, lt.topoColor2, currLerp);
@@ -40,12 +22,10 @@ public class CreateTopoMap : MonoBehaviour
             colors.Add(tileColor);
             currLerp += lerpStep;
         }
-        if (debug) Debug.Log("-----colors--------------------------------------------");
-        foreach (Color x in colors) if (debug) Debug.Log(x.ToString());
 
-        if (debug) Debug.Log("-----band values--------------------------------------------");
         // make list of bands
-        //this is working
+        List<float> bands = new List<float>();
+        float bandDistance = lt.depth / lt.numberOfTopoLevels;
         float val = 0;
         while (val < lt.depth)
         {
@@ -54,55 +34,29 @@ public class CreateTopoMap : MonoBehaviour
             bands.Add(lerpedval);
         }
 
-        foreach (float x in bands) if (debug) Debug.Log(x.ToString());
-        if (debug) Debug.Log("---------------------------------------------------------");
-         if (debug) Debug.Log("colors list: "+ colors.Count);
-         if (debug) Debug.Log("bands list: " + bands.Count);
-
-
-        List<string> cm = new List<string>();
-        int count = 0;
         // Creating colormap
+        Color[] colorMap = new Color[lt.X * lt.Y];
         for (int y = 0; y < lt.Y; y++)
         {
             for (int x = 0; x < lt.X; x++)
             {
                 float elevation = lt.finalMap.GetTile(x, y).ValuesHere[LayersEnum.Elevation];
-                
                 for (int j = 0; j < bands.Count; j++)
                 {
                     if (elevation < bands[j])
                     {
                         colorMap[x * lt.X + y] = colors[j];
-
-                        if (!cm.Contains(colorMap[count].ToString()))
-                        {
-                            cm.Add(colorMap[y].ToString());
-                        }
-                        
                         break; //dont need to check rest of bands
                     }
                 }
             }
         }
-        if (debug) Debug.Log("-----unique colors in colorMap before setting pixels---------");
-        if (debug) foreach (string x in cm) Debug.Log(x);
-        if (debug) Debug.Log("-----unique colors in texture before setting pixels---------");
 
-        List<string> good1 = new List<string>();
-        foreach (Color pixel in texture.GetPixels())
-            if (!good1.Contains(pixel.ToString()))
-            {
-                good1.Add(pixel.ToString());
-            }
-        if (debug) foreach (string x in good1) Debug.Log(x);
-        if (debug) Debug.Log("--------------------------------------------------------------");
-        if (debug) Debug.Log("Size of GetPixels()[] : "+texture.GetPixels().Length);
-        if (debug) Debug.Log("Size of colorMap[] : " + colorMap.Length);
-        if (debug) Debug.Log("colorMap[] is a : " + colorMap.GetType());
-
-
-        texture.SetPixels(colorMap);
+        // if this starts to break,
+        // make sure plane has existing material
+        Texture2D texture = new Texture2D(lt.X, lt.Y);
+        Renderer rend = topoObject.GetComponent<Renderer>();
+        texture.SetPixels(colorMap); //TODO: change to SetPixels32, it's faster. Especially if doing live reload
         texture.Apply();
         rend.material.mainTexture = texture;
     }
