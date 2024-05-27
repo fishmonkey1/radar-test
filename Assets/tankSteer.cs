d92b9864c35e3f218158025a358f6993e4439a7e
fishmonkey1 <11984660+fishmonkey1@users.noreply.github.com>
2024-05-27 09:16:55 -0400

    Better tank steering
    Idk seems good to me

--- Assets/tankSteer.cs
+++ Assets/tankSteer.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class tankSteer : MonoBehaviour
 {
-    [SerializeField] float speed = 5.0f;
+    [SerializeField] float maxSpeed = 50.0f;
+    [SerializeField] float maxReverseSpeed = -2.0f;
+    [SerializeField] float turnAngleMin = 20f;
+    [SerializeField] float turnAngleMax = 50f;
+    [SerializeField] float acceleration = 2.0f;
+    [SerializeField] float decceleration = 5f;
+    [SerializeField] float currSpeed;
+    [SerializeField] float turnAngle;
+
 
     void Update()
-    {
-        float turnAngle = Mathf.Clamp(.75f / speed, -.5f, .5f); //this needs to be edited to be better but it works
-        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * (speed*10f) * Time.deltaTime);
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);  
+    {   
+        if (Input.GetAxis("Vertical") != 0f)
+        {
+            currSpeed = Mathf.MoveTowards(currSpeed, maxSpeed * Input.GetAxis("Vertical"), acceleration * Time.deltaTime);   
+        } else 
+        {    //if not accel/decel, move speed towards 0
+            currSpeed = Mathf.MoveTowards(currSpeed, 0, decceleration * Time.deltaTime);
+        }
+        Mathf.Clamp(currSpeed, maxReverseSpeed, maxSpeed);
+
+        // The faster we are, the less we can turn
+        float turnAngleMaffs = turnAngleMax * (1f - (Mathf.InverseLerp(0, maxSpeed, Mathf.Abs(currSpeed))));
+        turnAngle = Mathf.Clamp(turnAngleMaffs, turnAngleMin, turnAngleMax);
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * turnAngle * Time.deltaTime);
+        transform.Translate(Vector3.forward * currSpeed * Time.deltaTime);
+
+
     }
 
 }
