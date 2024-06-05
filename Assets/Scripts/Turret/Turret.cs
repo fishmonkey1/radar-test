063e1d430c560a58385ee8d1ef6b7e93e22c70c2
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-06-05 19:01:16 -0400

    Stub Turret script and update controls
    The turret script is in progress and added, along with some controls that will be needed

---
+++ Assets/Scripts/Turret/Turret.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform turret; //The turret moves along the z axis
+    [SerializeField]
+    Transform barrel; //The barrel moves along the x axis
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
