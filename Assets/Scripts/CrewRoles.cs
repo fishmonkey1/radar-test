bd3a49391e3ccbf214d0d744aa59cb4f61ee67ed
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-06-05 19:12:59 -0400

    Create Crew Roles
    I'm using CrewRoles as a class containing constants for each of the crew definitions. I left a limit variable in there in case we decide to expand the roles to include ones players can share, but it's a little silly to have right now.

---
+++ Assets/Scripts/CrewRoles.cs
@@ -0,0 +1,23 @@
+public static class CrewRoles
+{
+    public static readonly Role Driver = new Role("Driver", 0, 1);
+    public static readonly Role Gunner = new Role("Gunner", 1, 1);
+    public static readonly Role Spotter = new Role("Spotter", 2, 1);
+    public static readonly Role Radar = new Role("Radar", 3, 1);
+
+
+}
+
+public class Role
+{
+    public string Name { get; private set; }
+    public uint ID { get; private set; }
+    public uint Limit { get; private set; }
+
+    public Role(string name, uint id, uint limit)
+    {
+        Name = name;
+        ID = id;
+        Limit = limit;
+    }
+}
