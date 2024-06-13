c6617de78ed1f02348bba9b05613c0ccb26860cf
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-06-12 22:44:00 -0400

    RolePicker Updates
    Okay, I've been tinkering around in the RolePicker class and I got something set up that I think ought to work. I haven't provided support for switching from one role to another, I just want to test this as it is right now. Testing soon once I get the network spawning the prefab.

--- Assets/Scripts/Network/RolePicker.cs
+++ Assets/Scripts/Network/RolePicker.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
@@ -13,6 +12,8 @@ public class RolePicker : NetworkBehaviour
     GameObject RoleButtonPrefab;
     public readonly SyncDictionary<Role, NetworkIdentity> pickedRoles = new();
 
+    List<GameObject> buttons = new List<GameObject>();
+
     void Start()
     {
         foreach (var role in CrewRoles.ImplementedRoles)
@@ -20,6 +21,54 @@ public class RolePicker : NetworkBehaviour
             GameObject newButton = GameObject.Instantiate(RoleButtonPrefab, RoleButtonPanel);
             TextMeshProUGUI buttonText = newButton.GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = role.Name;
+            Button buttonScript = newButton.GetComponent<Button>();
+            buttonScript.onClick.AddListener(() => SelectRole(role.Name));
+            buttons.Add(newButton);
+        }
+    }
+
+    void SelectRole(string roleName)
+    {
+        //Fetch the local owner
+        NetworkIdentity owner = NetworkClient.connection.identity;
+        CmdSelectRole(roleName, owner); //Send this to the server
+    }
+
+    [Command]
+    void CmdSelectRole(string roleName, NetworkIdentity owner)
+    { //Called on the server to pick a role and add to the SyncDict
+        Role pickedRole = CrewRoles.GetRoleByName(roleName);
+        pickedRoles.Add(pickedRole, owner);
+    }
+
+    public override void OnStartClient()
+    {
+        pickedRoles.Callback += OnPickedRoleChanged;
+    }
+
+    void OnPickedRoleChanged(SyncDictionary<Role, NetworkIdentity>.Operation op, Role role, NetworkIdentity owner)
+    { //Called on observing clients when the dictionary has changed
+        switch (op) 
+        {
+            case SyncIDictionary<Role, NetworkIdentity>.Operation.OP_ADD:
+                ChangeRoleButton(role, false); //Disable the picked role
+                break;
+            case SyncIDictionary<Role, NetworkIdentity>.Operation.OP_REMOVE:
+                ChangeRoleButton(role, true); //Enable the picked role since its removed
+                break;
+        }
+    }
+
+    void ChangeRoleButton(Role role, bool enable)
+    { //If disable is true then we turn the button off, and the opposite for false
+        foreach(GameObject button in buttons)
+        {
+            TextMeshProUGUI text = button.GetComponentInChildren<TextMeshProUGUI>();
+            if (text.text == role.Name)
+            { //We found it
+                button.SetActive(enable);
+                break;
+            }
         }
     }
 
