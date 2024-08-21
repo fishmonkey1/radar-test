11a38e207118f906da11587ef5add26ebd53e195
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-08-21 08:56:43 -0400

    EnemyThreatMap class
    There's a default map I made by hand and then loading and saving functions for editing things through JSON. I haven't actually hooked it up to anything yet, but it should be usable with the defaults for later.

--- Assets/Scripts/Enemy/EnemyType.cs
+++ Assets/Scripts/Enemy/EnemyType.cs
@@ -3,8 +3,8 @@ public enum EnemyType
     UNASSIGNED, //Note to future Vicky: Use this value as human error catching.
     INFANTRY, //Maid infantry anyone?
     TRANSPORT_TRUCK, //Like a covered truck from WW2, infantry transport
-    BTR, //Part troop transport, part light tank
     APC, //Basically an armored transport truck
+    BTR, //Part troop transport, part light tank
     ROCKET_TRUCK, //Missile/rocket loaded truck like the old soviet ones
     RADAR_TRUCK, //Mobile radar emplacement
     LIGHT_TANK, //Like recon tanks in Hell Let Loose
