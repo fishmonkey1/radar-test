a687315890f7ca0dea50dd31992f957219bcd087
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-08-21 07:32:51 -0400

    Moar delegates!
    Moved the enemy script and added some logging/initialization for the delegates I crammed into the enemy manager.

--- Assets/Scripts/Network/Enemy/EnemyManager.cs
+++ Assets/Scripts/Network/Enemy/EnemyManager.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
@@ -17,11 +15,13 @@ public class EnemyManager : MonoBehaviour
         HIGH,
         LOCKDOWN
     }
-    public AlertLevel Alert {  get; private set; }
+    public AlertLevel Alert { get; private set; }
+    public delegate void AlertLevelDelegate(AlertLevel alertLevel);
+    public AlertLevelDelegate OnAlertLevelChanged;
 
     public enum SuspicionLevel
     { //If the player gets spotted frequently but doesn't get caught the SuspicionLevel goes up
-      ///potentially raises AlertLevel
+        ///potentially raises AlertLevel
         NONE,
         SLIGHT,
         SEARCH,
@@ -29,6 +29,9 @@ public class EnemyManager : MonoBehaviour
         ALERT, //This raises the AlertLevel and drops suspicion to search
     }
     public SuspicionLevel Suspicion { get; private set; }
+    public delegate void OnSuspicionLevelChanged(SuspicionLevel suspicion);
+    public OnSuspicionLevelChanged SuspicionLevelChanged;
+
 
     /// <summary>
     /// If a player has been spotted, this is true. Could potentially raise the alert level if spotted again
@@ -37,10 +40,13 @@ public class EnemyManager : MonoBehaviour
     /// <summary>
     /// How long the player should avoid enemies for the investigation to end only in suspicion
     /// </summary>
-    public float InvestigateTimeLeft { get; private set; } 
+    public float InvestigateTimeLeft { get; private set; }
+    public delegate void OnInvestigateChanged(bool isInvestigating);
+    public OnInvestigateChanged InvestigateChanged;
 
+    [System.Serializable]
     public struct Vector3FloatPair
-    {
+    { 
         public float number;
         public Vector3 vector;
     }
@@ -61,8 +67,31 @@ public class EnemyManager : MonoBehaviour
             Debug.Log("You have two EnemyManagers in the scene, disabling this one.");
             enabled = false; //Turn the script off and let somebody know
         }
+        OnAlertLevelChanged = new AlertLevelDelegate(LogAlertChanged);
+        SuspicionLevelChanged = new OnSuspicionLevelChanged(LogSupsicionLevel);
+        InvestigateChanged = new OnInvestigateChanged(LogInvestigateState);
+        OnEnemySpawned = new OnEnemySpawn(LogEnemySpawn);
     }
 
+    void LogAlertChanged(AlertLevel level)
+    {
+        Debug.Log("Alert level changed. New alert level is: " + level.ToString());
+    }
 
+    void LogSupsicionLevel(SuspicionLevel level)
+    {
+        Debug.Log("Suspicion level changed. New suspicion level is " + level.ToString());
+    }
+
+    void LogInvestigateState(bool investigating)
+    {
+        Debug.Log("Investigation has changed. New investigate value is " + investigating);
+    }
+
+    GameObject LogEnemySpawn(Enemy enemy)
+    {
+        Debug.Log("A new enemy has spawned.");
+        return enemy.gameObject; //I don't entirely remember why I was doing it this way...
+    }
 
 }
