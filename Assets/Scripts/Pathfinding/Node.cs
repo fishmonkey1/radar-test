3779ab9a803ae6987b3076109992e04a82028919
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-09-08 19:46:04 -0400

    Node updates and Traversal enum
    There's now an enum for marking nodes as only navigated by particular types of enemies, defaulting to NOT_SET so the map creator can do it as they build out the level

--- Assets/Scripts/Pathfinding/Node.cs
+++ Assets/Scripts/Pathfinding/Node.cs
@@ -5,4 +5,7 @@ public class Node : MonoBehaviour
 {
     public List<Node> Connections = new(); //Other nodes reachable from this one
     public bool StartOfNewRoad; //If this needs to have another road renderer used
+    //The AI uses this to determine if this node should be weighted higher for pathfinding, for use with highways for example
+    public uint NodeImportance;
+    public NodeTraversal NodeTraversal = NodeTraversal.NOT_SET;
 }
---
+++ Assets/Scripts/Pathfinding/NodeTraversal.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NodeTraversal
+{
+    NOT_SET, //Used as error handling, meaning you didn't assign anything to the node
+    INFANTRY, //If this node can't fit aircraft or vehicles than only infantry move through it
+    LIGHT_VEHICLE, //Handles light vehicles only, can't fit heavy tanks and such. Excludes aircraft
+    HEAVY_VEHICLE, //Only used for the big boi tanks, though I can't think of an example so this may get axed
+    AIRCRAFT, //This node is used for aircraft pathing and not for ground movement
+    ANY_GROUND, //Any ground based movement can traverse this node
+    ANY_VEHICLE //Any vehicle can move along this node, excluding infantry
+}
