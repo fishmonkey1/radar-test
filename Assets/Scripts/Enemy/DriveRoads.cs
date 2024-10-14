380405744301bc9a5de6c373ecaae524d318b14b
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-10-14 12:03:18 -0400

    New navigation attempt, who dis?
    Okay, I wasn't getting good results trying to cannibalize the PathFollower example, so I'm trying something else out now with driving the enemy around based on path points. This is untested, but that's for after lunch. :3

--- Assets/Scripts/Enemy/DriveRoads.cs
+++ Assets/Scripts/Enemy/DriveRoads.cs
@@ -1,21 +1,20 @@
-using PathCreation;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
+using System.IO;
+using UnityEditor;
 
-[RequireComponent(typeof(Enemy))] //This should only go on enemies
-public class NavigateRoads : Navigation
+public class DriveRoads : Navigation
 {
-    public float MoveSpeed;
+
     NodesToRoads roadNodes; //The road we're on and trying to follow
     PathCreator roadPath;
-    List<Node> path; //Contains all the road nodes we want to travel through to get to Enemy.targetNode
     Node nextNode; //The next node we're trying to reach
     [SerializeField] float rangeCheck = 0.5f; //How far the transform can be before we count it as reached
     [SerializeField] float rangeInterval = 0.5f; //How often we check to see if we're near the node
     EndOfPathInstruction endOfPathInstruction;
-
-    float distanceTravelled = 0f;
+    PatrolOrder patrolOrder; //Store a reference to this since I expect most usage of this script to be from patrols
 
     public void Initialize()
     {
@@ -23,37 +22,36 @@ public class NavigateRoads : Navigation
         squad = owner.Squad; //Nab the squad so we can follow our orders
         active = true; //I'm just assuming that these won't need to be turned off for now
 
-        //Now we need to locate our NodesToRoads object
         roadNodes = GetComponent<Enemy>().NearNode.NodeRenderer; //Find the renderer from the node we've been teleported to by the spawner
         roadPath = roadNodes.GetPathCreator();
 
         if (squad.OrderContext.Order == Orders.PATROL)
-        { //We should grab the path since we're doing a patrol
+        { //We should grab the path since we're likely doing a patrol
             //Future Vicky, I'm pretty sure the path is already populated from the OrdersManager
-            PatrolOrder patrol = squad.OrderContext as PatrolOrder;
-            path = patrol.Nodes;
-            nextNode = patrol.Node; //Assign the first node as our target and let the update function mark it as reached
-            if (patrol.Looping) //If we loop then actually set the instruction to reverse
+            patrolOrder = squad.OrderContext as PatrolOrder;
+            nextNode = patrolOrder.Nodes[1]; //Assign the second node in the list to be the first to go towards
+            if (patrolOrder.Looping) //If we loop then actually set the instruction to reverse
                 endOfPathInstruction = EndOfPathInstruction.Reverse;
             else
                 endOfPathInstruction = EndOfPathInstruction.Stop;
         }
-        StartCoroutine(CheckDistanceToNode()); //Start the distance check
+        //Now we need to rotate our unit to face the node it's going to
+        transform.LookAt(nextNode.transform.position); //Point towards the node
+        StartCoroutine(CheckDistanceToNextNode()); //Start the distance check
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (active)
-        {
-            distanceTravelled += MoveSpeed * Time.deltaTime;
-            transform.position = roadPath.path.GetPointAtDistance(distanceTravelled, endOfPathInstruction);
-            //transform.rotation = roadPath.path.GetRotationAtDistance(distanceTravelled, endOfPathInstruction);
-        }
+        //Get the point the truck is going to move to
+        Vector3 aheadPoint = transform.position + transform.forward * owner.MaxSpeed * Time.deltaTime;
+        //Now we find the closest point on the path to our aheadPoint
+        Vector3 pathPoint = roadPath.path.GetClosestPointOnPath(aheadPoint);
+        transform.LookAt(pathPoint); //Orientate to face the point on the path, then move towards it
+        transform.position += transform.forward * owner.MaxSpeed * Time.deltaTime;
     }
 
-    IEnumerator CheckDistanceToNode()
-    { //Gets checked every rangeInterval seconds and updates the next node
+    public IEnumerator CheckDistanceToNextNode()
+    {
         while (true)
         {
             float distance = Vector3.Distance(transform.position, nextNode.transform.position);
@@ -68,7 +66,6 @@ public class NavigateRoads : Navigation
                 { //This means we're at an intersection and need to continue down a new road
                     roadNodes = nextNode.NodeRenderer;
                     roadPath = roadNodes.GetPathCreator();
-                    distanceTravelled = 0;
                     Debug.Log("Next node has a different renderer, reassigning...");
                 }
             }
