f8196822107eac271eaba69d663dbfbd509e997f
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-10-27 15:56:10 -0400

    Ditch steering, attempt turning slerps
    I'm still fiddling with getting these trucks moving around the map. I still need to get the spawner to place the trucks in larger squads later too... Adding some debug toggles next to speed up my process

--- Assets/Scripts/Enemy/NavigateRoads.cs
+++ Assets/Scripts/Enemy/NavigateRoads.cs
@@ -7,11 +7,12 @@ public class NavigateRoads : Navigation
 
     NodesToRoads roadNodes; //The road we're on and trying to follow
     PathCreator roadPath;
-    Node nextNode; //The next node we're trying to reach
+    [SerializeField] Node nextNode; //The next node we're trying to reach
     [SerializeField] float rangeCheck = 0.5f; //How far the transform can be before we count it as reached
     [SerializeField] float rangeInterval = 0.5f; //How often we check to see if we're near the node
+    [SerializeField] float maxTurnAngle = 35f;
+    [SerializeField] float turnSpeed = 2f;
     PatrolOrder patrolOrder; //Store a reference to this since I expect most usage of this script to be from patrols
-    Steering steer;
 
     public void Initialize()
     {
@@ -30,7 +31,6 @@ public class NavigateRoads : Navigation
         }
         //Now we need to rotate our unit to face the node it's going to
         transform.LookAt(nextNode.transform.position); //Point towards the node
-        steer = GetComponent<Steering>();
         StartCoroutine(CheckDistanceToNextNode()); //Start the distance check
     }
 
@@ -41,9 +41,19 @@ public class NavigateRoads : Navigation
         //Now we find the closest point on the path to our aheadPoint
         //If we changed road renderers, the unit should be facing the next node, which should prevent wrong turns
         Vector3 pathPoint = roadPath.path.GetClosestPointOnPath(aheadPoint);
-        transform.LookAt(pathPoint); //Orientate to face the point on the path, then move towards it
+        Vector3 directionToPoint = pathPoint - transform.position;
+        float angleToPoint = Vector3.Angle(transform.forward, directionToPoint);
+        if (angleToPoint > maxTurnAngle)
+        { //If the next found point is outside of our turning range, start turning to face it
+            Quaternion targetRotation = Quaternion.LookRotation(directionToPoint);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+        }
+        else
+            transform.LookAt(pathPoint); //Orientate to face the point on the path, then move towards it
+
+        //Now move the vehicle forwards
         transform.position += transform.forward * owner.MaxSpeed * Time.deltaTime;
-        //steer.UpdateSteering(pathPoint);
+        
     }
 
     public IEnumerator CheckDistanceToNextNode()
