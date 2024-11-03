a164ada106f73fb54d72eddec2a5a5fa90eda6a5
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-11-03 18:43:26 -0500

    Update Chat.cs
    Add MessageType support to chat. This allows for messages to be sent on other channels, soon to be used by the server for player connection messages.

--- Assets/Scripts/Network/Chat.cs
+++ Assets/Scripts/Network/Chat.cs
@@ -30,6 +30,9 @@ public class Chat : NetworkBehaviour
         }
     }
 
+    /// <summary>
+    /// Enum for the different chat message channels that can be used. For the life of me, I can't remember why I marked this with Flags...
+    /// </summary>
     [Flags, Serializable]
     public enum MessageTypes
     {
@@ -43,17 +46,23 @@ public class Chat : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        chatInput.onSubmit.AddListener((text) => SendChatMessage(text));
+        //TODO: Add variables to monitor which channels the Chat script is listening to.
+        chatInput.onSubmit.AddListener((text) => SendChatMessage(text, MessageTypes.ALL)); //We default all messages to the ALL channel until more chat support is written
     }
 
-    void SendChatMessage(string text)
+    /// <summary>
+    /// This function is triggered by the Chat script's InputField and captures the text and the channel it is meant to go on.
+    /// </summary>
+    /// <param name="text">The message to be sent over the network</param>
+    /// <param name="messageType">Which channel to send the message on</param>
+    void SendChatMessage(string text, MessageTypes messageType)
     { //This happens on the local machine, giving us a chance to collect info before sending to the server
         chatInput.text = ""; //Clear the text out of the inputField
-        CmdSendChatMessage(text, NetworkClient.connection.connectionId, PlayerInfo.localPlayerName);
+        CmdSendChatMessage(text, NetworkClient.connection.connectionId, PlayerInfo.localPlayerName, messageType);
     }
 
     [Command(requiresAuthority = false)]
-    public void CmdSendChatMessage(string text, int connectionID, string playerName)
+    public void CmdSendChatMessage(string text, int connectionID, string playerName, MessageTypes messageType)
     { //Server finds the player's name, appends the text to make the whole message, then sends the message to all clients
         ChatMessage message = new ChatMessage(text, playerName, connectionID, MessageTypes.ALL); //Just default to all for now
         chatText.text += BuildMessage(message); //Stick this on the end of the server's chatText area
