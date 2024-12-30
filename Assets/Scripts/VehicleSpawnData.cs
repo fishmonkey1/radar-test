c80e1990a327c37703974d38377eff403a11b291
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2024-12-30 12:43:49 -0500

    Update VehicleSpawnData.cs
    Lemme make this a list for now and I'll think about how I wanna link this for vehicles to have later. Gonna focus on spawning multiple tanks first

--- Assets/Scripts/VehicleSpawnData.cs
+++ Assets/Scripts/VehicleSpawnData.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 
 public class VehicleSpawnData : MonoBehaviour
 {
-    List<Role> VehicleRoles = new(); //This tracks all of the roles that this vehicle has, and how many
-    List<PlayerProfile> PlayerProfiles = new(); //The profiles that are assigned to this vehicle
-    [SerializeField]
-    GameObject VehiclePrefab; //The GameObject to instantiate for this vehicle
+    public List<VehicleData> VehicleData = new();
+}
+
+[System.Serializable]
+public class VehicleData
+{
+    public List<Role> VehicleRoles = new(); //This tracks all of the roles that this vehicle has, and how many
+    public List<PlayerProfile> PlayerProfiles = new(); //The profiles that are assigned to this vehicle
+    public GameObject VehiclePrefab; //The GameObject to instantiate for this vehicle
 }
