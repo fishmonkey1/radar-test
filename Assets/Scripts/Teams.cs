7597550457bc3594b9df3721306829b559874fa9
Victoria Eastep <114838742+BarndoBoi@users.noreply.github.com>
2025-01-01 16:17:20 -0500

    Framework for making teams
    I decided to go ahead and slap together some crap in a singleton class where I can track which teams are in the game. I'm thinking of building the teams in the inspector for now, but this will be used with the RolePicker rewrite soon

---
+++ Assets/Scripts/Teams.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HorniTank
+{
+    public class Teams
+    {
+        /// <summary>
+        /// There is only one instance of Teams, so we use a simple singelton pattern
+        /// </summary>
+        static Teams instance;
+        public static Teams Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Teams();
+                }
+                return instance;
+            }
+        }
+        public List<TeamInfo> AllTeams = new();
+
+        public static bool CheckTeamsHostile(TeamInfo firstTeam, TeamInfo secondTeam)
+        {
+            if (firstTeam.EnemyTeamIds.Contains(secondTeam.TeamId) || secondTeam.EnemyTeamIds.Contains(firstTeam.TeamId))
+            {
+                return true; //At least one group is hostile to the other
+            }
+            return false; //Otherwise they aren't hostile to each other
+        }
+
+        public static bool CheckTeamsFriendly(TeamInfo firstTeam, TeamInfo secondTeam)
+        {
+            if (firstTeam.FriendlyTeamIds.Contains(secondTeam.TeamId) || secondTeam.FriendlyTeamIds.Contains(secondTeam.TeamId))
+            {
+                return true; //This teams are friendly to each other
+            }
+            return false; //Otherwise they are not friendly to each other
+        }
+
+        public static bool CheckTeamsNeutral(TeamInfo firstTeam, TeamInfo secondTeam)
+        {
+            if (CheckTeamsHostile(firstTeam, secondTeam) != true && CheckTeamsFriendly(firstTeam, secondTeam) != true)
+            {
+                return true; //These teams are neutral to each other
+            }
+            return false;
+        }
+
+    }
+
+    public class TeamInfo
+    {
+        public string TeamName;
+        /// <summary>
+        /// TeamId is used as a primary key when determining which team we're trying to find.
+        /// TODO: Consider turning this into a byte later on
+        /// </summary>
+        public uint TeamId;
+        /// <summary>
+        /// The number of players that can be on a team. Defaults to 32 for now.
+        /// TODO: Consider changing this to a byte later on
+        /// </summary>
+        public uint TeamLimit = 32;
+        public List<uint> EnemyTeamIds = new();
+        public List<uint> FriendlyTeamIds = new();
+    }
+}
