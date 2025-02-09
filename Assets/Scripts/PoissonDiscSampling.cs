f30058fbd5d5d306f1734da7f1d6cfe1e78b0ccd
fishmonkey1 <11984660+fishmonkey1@users.noreply.github.com>
2025-02-09 12:27:20 -0500

    checkpoint

--- Assets/Scripts/PoissonDiscSampling.cs
+++ Assets/Scripts/PoissonDiscSampling.cs
@@ -40,6 +40,54 @@ public static class PoissonDiscSampling {
 		return points;
 	}
 
+	public static List<Vector2> GeneratePointsPerlin(float radius, Vector2 sampleRegionSize, int numSamplesBeforeRejection = 30, float[] perlinNoise = null)
+	{	
+
+		/*
+		use the maximum radius for the cell size, not min
+		run perlin get noise map
+		check distance against value of underlying for the radius at each stop
+
+		 
+		 
+		 */
+		float cellSize = radius / Mathf.Sqrt(2);
+
+		int[,] grid = new int[Mathf.CeilToInt(sampleRegionSize.x / cellSize), Mathf.CeilToInt(sampleRegionSize.y / cellSize)];
+		List<Vector2> points = new List<Vector2>();
+		List<Vector2> spawnPoints = new List<Vector2>();
+
+		spawnPoints.Add(sampleRegionSize / 2);
+		while (spawnPoints.Count > 0)
+		{
+			int spawnIndex = Random.Range(0, spawnPoints.Count);
+			Vector2 spawnCentre = spawnPoints[spawnIndex];
+			bool candidateAccepted = false;
+
+			for (int i = 0; i < numSamplesBeforeRejection; i++)
+			{
+				float angle = Random.value * Mathf.PI * 2;
+				Vector2 dir = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+				Vector2 candidate = spawnCentre + dir * Random.Range(radius, 2 * radius);
+				if (IsValid(candidate, sampleRegionSize, cellSize, radius, points, grid))
+				{
+					points.Add(candidate);
+					spawnPoints.Add(candidate);
+					grid[(int)(candidate.x / cellSize), (int)(candidate.y / cellSize)] = points.Count;
+					candidateAccepted = true;
+					break;
+				}
+			}
+			if (!candidateAccepted)
+			{
+				spawnPoints.RemoveAt(spawnIndex);
+			}
+
+		}
+
+		return points;
+	}
+
 	static bool IsValid(Vector2 candidate, Vector2 sampleRegionSize, float cellSize, float radius, List<Vector2> points, int[,] grid) {
 		if (candidate.x >=0 && candidate.x < sampleRegionSize.x && candidate.y >= 0 && candidate.y < sampleRegionSize.y) {
 			int cellX = (int)(candidate.x/cellSize);
